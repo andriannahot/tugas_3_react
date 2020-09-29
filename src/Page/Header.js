2678822b4eabd881c36bde9324992cd84a18f93e
andrian <64577548+andriannahot@users.noreply.github.com>
2020-09-29 08:24:00 +0700

    Add files via upload

---
+++ src/Page/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+	render(){
+		return(
+			<div>
+			<center></center>
+			<h3>Home| Product | Kontak | Tentang Kami</h3></div>)
+	}
+}
+
+export default Header;
